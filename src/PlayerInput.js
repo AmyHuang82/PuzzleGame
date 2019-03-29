43d19a964d14d2a0ca661c2a85e96d04d6a982f3
Amy Huang <a3315520@gmail.com>
2019-03-29 16:36:50 +0800

    change input & button style

--- src/PlayerInput.js
+++ src/PlayerInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PlayerInput(props) {
     return (
-        <div>
+        <div className="player-input">
             <input placeholder="Enter Your Name" onChange={props.playerNameInput} value={props.value} />
             <button onClick={props.shuffleNumber}>Start</button>
         </div>
