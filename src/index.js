143ff2fd632c1604241b4d2f3a921ab791bbffd2
Amy Huang <a3315520@gmail.com>
2019-03-29 18:00:17 +0800

    add Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './App';
+import RankingShow from './RankingShow';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Link to="/" className="category">Game</Link>
+                <Link to="/ranking" className="category">Rank</Link>
+
+
+                <Route path="/" exact render={
+                    () => {
+                        return (<App />);
+                    }
+                } />
+
+                <Route path="/ranking" exact render={
+                    () => {
+                        return (<RankingShow />);
+                    }
+                } />
+            </Router>
+        );
+    }
+}
 
 window.addEventListener('load', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(<Main />, document.getElementById('root'));
 });
