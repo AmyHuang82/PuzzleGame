86eb74c73c1c7eda0c9fd63dd74f740f2d96b477
Amy Huang <a3315520@gmail.com>
2019-04-03 09:24:23 +0800

    add # to basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import RankingShow from './RankingShow';
 class Main extends React.Component {
     render() {
         return (
-            <Router basename={"/PuzzleGame"}>
+            // 加上＃可以讓網頁不會重新導入到其他頁面重刷就不會出錯
+            <Router basename={"/PuzzleGame/#/"}>
                 <NavLink to="/" exact className="category" activeStyle={{ backgroundColor: "rgb(165, 165, 165)", color: "white" }}>Game</NavLink>
                 <NavLink to="/ranking" exact className="category" activeStyle={{ backgroundColor: "rgb(165, 165, 165)", color: "white" }}>Rank</NavLink>
 
