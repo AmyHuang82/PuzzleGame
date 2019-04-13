8f705114ea51f501cdc37fbfaa64b2b969a30219
Amy Huang <a3315520@gmail.com>
2019-04-14 00:00:15 +0800

    fixed router problem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import App from './App';
 import RankingShow from './RankingShow';
 
@@ -8,7 +8,7 @@ class Main extends React.Component {
     render() {
         return (
             // 加上＃可以讓網頁不會重新導入到其他頁面重刷就不會出錯
-            <Router basename={"/PuzzleGame/#"}>
+            <Router basename={"/PuzzleGame/"}>
                 <NavLink to="/" exact className="category" activeStyle={{ backgroundColor: "rgb(165, 165, 165)", color: "white" }}>Game</NavLink>
                 <NavLink to="/ranking" exact className="category" activeStyle={{ backgroundColor: "rgb(165, 165, 165)", color: "white" }}>Rank</NavLink>
 
